2fd20704662b5e82c4315e678cf79936e7d5850c
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2024-10-30 01:09:01 +0900

    feat SessionStatus enum 클래스 추가

--- src/main/java/nextstep/courses/domain/Session.java
+++ src/main/java/nextstep/courses/domain/Session.java
@@ -1,43 +1,96 @@
 package nextstep.courses.domain;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import nextstep.payments.domain.Payment;
 import nextstep.users.domain.NsUser;
 
 public class Session {
-    private final Long id;
+    private final Long sessionId;
     private final Image image;
     private final SessionType sessionType;
-    private final List<NsUser> Students = new ArrayList<>();
-    int price;
-    int maxStudents;
+    private final SessionStatus sessionStatus;
+    private final List<NsUser> students = new ArrayList<>();
+    private final List<Payment> payments = new ArrayList<>();
+    private final LocalDateTime startDate = LocalDateTime.now();
 
-    public static Session createSession(Long id, Image image, SessionType sessionType, int price, int maxStudents) {
-        if(sessionType == SessionType.PAID) {
-            return new Session(id, image, sessionType, price, maxStudents);
+    private LocalDateTime endDate;
+    private long price;
+    private int maxStudents;
+
+
+    public static Session createSession(Long id, Image image, SessionType sessionType, SessionStatus sessionStatus,
+                                        int price, int maxStudents) {
+        if (sessionType == SessionType.PAID) {
+            return new Session(id, image, sessionType, sessionStatus, price, maxStudents);
         }
-        return new Session(id, image, sessionType);
+        return new Session(id, image, sessionType, sessionStatus);
     }
 
-    private Session(Long id, Image image, SessionType sessionType, int price, int maxStudents) {
-        this.id = id;
+    private Session(Long sessionId, Image image, SessionType sessionType, SessionStatus sessionStatus, int price,
+                    int maxStudents) {
+        this.sessionId = sessionId;
         this.image = image;
         this.sessionType = sessionType;
-        sessionType.validate(price,maxStudents);
+        sessionType.validate(price, maxStudents);
+        this.sessionStatus = sessionStatus;
         this.price = price;
         this.maxStudents = maxStudents;
     }
 
-    private Session(Long id, Image image, SessionType sessionType) {
-        this(id, image, sessionType, 0, Integer.MAX_VALUE);
+    private Session(Long sessionId, Image image, SessionType sessionType, SessionStatus sessionStatus) {
+        this(sessionId, image, sessionType, sessionStatus, 0, Integer.MAX_VALUE);
     }
 
-    int getPrice() {
+    public long getPrice() {
         return this.price;
     }
 
-    int getMaxStudents() {
+    public int getMaxStudents() {
         return this.maxStudents;
+    }
+
+    public void register(NsUser nsUser, long payment) {
+        registterStudent(nsUser, payment);
+        addPaymentHistory(nsUser, payment);
+    }
+
+    private void addPaymentHistory(NsUser nsUser, long payment) {
+        payments.add(new Payment("", sessionId, nsUser.getId(), payment));
+    }
+
+    public void registterStudent(NsUser nsUser, long payment) {
+        checkSessionIsRegistering();
+        checkPaymentEqualsPrice(payment);
+        checkCurrentNumberOfStudentsMax(getNumberOfStudents());
+        students.add(nsUser);
+    }
+
+    private void checkCurrentNumberOfStudentsMax(int numberOfStudents) {
+        if (numberOfStudents == maxStudents) {
+            throw new IllegalArgumentException("수강 정원이 모두 찼습니다.");
+        }
+    }
+
+    private void checkSessionIsRegistering() {
+        if (sessionStatus != SessionStatus.REGISTER) {
+            throw new IllegalArgumentException("이 강의는 지금 모집중인 상태가 아닙니다");
+        }
+    }
+
+    private void checkPaymentEqualsPrice(long payment) {
+        if (sessionType == SessionType.PAID && price != payment) {
+            throw new IllegalArgumentException("수강료와 가격이 일치하지 않습니다");
+        }
+    }
+
+    public int getNumberOfStudents() {
+        return students.size();
+    }
+
+    public int getNumberOfPayments() {
+        return payments.size();
     }
 
 }
--- src/test/java/nextstep/courses/domain/SessionTest.java
+++ src/test/java/nextstep/courses/domain/SessionTest.java
@@ -1,8 +1,13 @@
 package nextstep.courses.domain;
 
+import nextstep.users.domain.NsUserTest;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.EnumSource.Mode;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SessionTest {
 
@@ -12,7 +17,7 @@ public class SessionTest {
     @DisplayName("유료 강의가 잘 생성되는지 확인한다")
     void 유료_강의_생성() {
         Assertions.assertThatCode(() ->
-                Session.createSession(0L,image,SessionType.PAID, 1, 1));
+                Session.createSession(0L,image,SessionType.PAID, SessionStatus.READY, 1, 1));
 
     }
 
@@ -20,7 +25,7 @@ public class SessionTest {
     @DisplayName("유료 강의가 0원(무료이면) 이하 이면 예외가 발생한다")
     void 유료_강의_가격이_0원_이하일_시_예외_발생() {
         Assertions.assertThatThrownBy(() ->
-                Session.createSession(0L,image,SessionType.PAID, 0, 1))
+                Session.createSession(0L,image,SessionType.PAID,SessionStatus.READY, 0, 1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("유료강의의 강의는 1원 이상이어야 합니다.");
 
@@ -29,7 +34,7 @@ public class SessionTest {
     @DisplayName("유료 강의가 0원(무료이면) 이하 이면 예외가 발생한다")
     void 유료_강의_학생_수가_0명_이하일_시_예외_발생() {
         Assertions.assertThatThrownBy(() ->
-                Session.createSession(0L,image,SessionType.PAID, 1, 0))
+                Session.createSession(0L,image,SessionType.PAID,SessionStatus.READY, 1, 0))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("유료 강의의 최대 수강인원은 0명 이하일 수 없습니다");
 
@@ -39,14 +44,14 @@ public class SessionTest {
     @DisplayName("무료 강의가 잘 생성되는지 확인한다")
     void 무료_강의_생성() {
         Assertions.assertThatCode(() ->
-                Session.createSession(0L,image,SessionType.FREE,0,0));
+                Session.createSession(0L,image,SessionType.FREE,SessionStatus.READY,0,0));
     }
 
     @Test
     @DisplayName("무료 강의의 가격이 0인지 확인한다")
     void 무료_강의_가격_0원_인지_확인() {
         Assertions.assertThat(
-                Session.createSession(0L,image,SessionType.FREE,0,0).getPrice())
+                Session.createSession(0L,image,SessionType.FREE,SessionStatus.READY,0,0).getPrice())
                 .isEqualTo(0);
     }
 
@@ -54,8 +59,81 @@ public class SessionTest {
     @DisplayName("무료 강의의 수한생 제한이 없는지 (Integer.MAX_VALUE)인지 확인")
     void 무료_강의_수강생_제한_확인() {
         Assertions.assertThat(
-                        Session.createSession(0L,image,SessionType.FREE,0,0).getMaxStudents())
+                        Session.createSession(0L,image,SessionType.FREE,SessionStatus.READY,0,0).getMaxStudents())
                 .isEqualTo(Integer.MAX_VALUE);
+    }
+
+    @Test
+    @DisplayName("모집중인 강의에 수강신청을 할 수 있는지 검증한다")
+    void 모집중인_강의_수강신청_테스트() {
+        Session session = Session.createSession(0L,image,SessionType.PAID, SessionStatus.REGISTER, 100, 1);
+
+        session.registterStudent(NsUserTest.JAVAJIGI,100);
+
+        Assertions.assertThat(session.getNumberOfStudents()).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {100, 300})
+    @DisplayName("수강료가 일치하지 않을 경우 강의신청을 할 수 없다")
+    void 수강료_일치하지_않을_경우_예외_발생(int testCase) {
+        Session session = Session.createSession(0L,image,SessionType.PAID, SessionStatus.REGISTER, 200, 1);
+
+
+        Assertions.assertThatThrownBy(
+                () -> session.registterStudent(NsUserTest.JAVAJIGI,testCase)
+        ).isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("수강료와 가격이 일치하지 않습니다");
+
+    }
+
+    @ParameterizedTest
+    @EnumSource(mode = Mode.EXCLUDE, names = {"REGISTER"})
+    @DisplayName("모집 중인 강의를 수강신청하면 예외가 발생한다")
+    void 모집중이지_않은_강의_수강신청_불가(SessionStatus status) {
+        Session session = Session.createSession(0L,image,SessionType.PAID, status, 200, 1);
+
+        Assertions.assertThatThrownBy(
+                () -> session.registterStudent(NsUserTest.JAVAJIGI,200)
+                ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이 강의는 지금 모집중인 상태가 아닙니다");
+    }
+
+    @Test
+    @DisplayName("유료강의 현재 수강정원이 최대인원일 시 예외 발생")
+    void 유료강의_수강인원_최대일_시_예외_발생() {
+        Session session = Session.createSession(0L,image,SessionType.PAID, SessionStatus.REGISTER, 200, 1);
+
+        session.registterStudent(NsUserTest.JAVAJIGI,200);
+
+        Assertions.assertThatThrownBy(
+                        () -> session.registterStudent(NsUserTest.JAVAJIGI,200)
+                ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("수강 정원이 모두 찼습니다.");
+    }
+
+    @Test
+    @DisplayName("무료강의 수강인원 제한 없음(Integer.MAX_VALUE)")
+    void 무료강의_수강인원_제한_없음() {
+        Session session = Session.createSession(0L,image,SessionType.FREE, SessionStatus.REGISTER, 200, 0);
+
+        session.registterStudent(NsUserTest.JAVAJIGI,200);
+
+        Assertions.assertThatCode(
+                () -> session.registterStudent(NsUserTest.JAVAJIGI,200)
+                );
+    }
+
+    @Test
+    @DisplayName("결제정보가 잘 저장되는지 확인한다")
+    void 결제정보_저장_확인() {
+        Session session = Session.createSession(0L,image,SessionType.PAID, SessionStatus.REGISTER, 200, 1);
+
+        session.register(NsUserTest.JAVAJIGI,200);
+
+        Assertions.assertThat(
+                session.getNumberOfPayments()
+                ).isEqualTo(1);
     }
 
 }
