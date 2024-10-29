43f6672b9ca516a335e8b9e06ba99457f7034eeb
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2024-10-29 14:24:53 +0900

    Image 타입 추가

--- src/main/java/nextstep/courses/domain/ImageType.java
+++ src/main/java/nextstep/courses/domain/ImageType.java
@@ -1,5 +1,25 @@
 package nextstep.courses.domain;
 
 public enum ImageType {
-    GIF, JPG, JPEG, PNG, SVG
+    GIF,
+    JPG,
+    JPEG,
+    PNG,
+    SVG;
+
+    public static ImageType typeCheck(String imageType) {
+        validImageTypeIsNull(imageType);
+        try {
+            return ImageType.valueOf(imageType);
+        } catch(IllegalArgumentException e) {
+            throw new IllegalArgumentException("지원하지 않는 이미지 타입");
+        }
+    }
+
+    private static void validImageTypeIsNull(String imageType) {
+        if(imageType == null) {
+            throw new IllegalArgumentException("imageType 오류 (null)");
+        }
+    }
+
 }
--- src/test/java/nextstep/courses/domain/ImageSizeTest.java
+++ src/test/java/nextstep/courses/domain/ImageSizeTest.java
@@ -17,7 +17,7 @@ public class ImageSizeTest {
     @Test
     @DisplayName("이미지의 크기가 1MB를 넘어가면 예외가 발생한다")
     void 이미지_1MB_초과_시_예외() {
-        Assertions.assertThatCode(() -> new ImageSize(1024 * 1024+1))
+        Assertions.assertThatThrownBy(() -> new ImageSize(1024 * 1024+1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("현재 이미지 사이즈 1048577 1MB를 초과합니다");
     }
@@ -25,7 +25,7 @@ public class ImageSizeTest {
     @Test
     @DisplayName("이미지의 크기가 0이하면 예외가 발생한다")
     void 이미지_크기_0_이하일_시_예외() {
-        Assertions.assertThatCode(() -> new ImageSize(0))
+        Assertions.assertThatThrownBy(() -> new ImageSize(0))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이미지의 크기는 1바이트 이상이어야 합니다.");
     }
